96bc56c819678018015573da92e601b705ecc620
Casmir Okorie <casmir456@gmail.com>
2023-07-20 16:24:43 +0100

    Moving my work to github

---
+++ index.js
@@ -0,0 +1,45 @@
+var emailfield = document.getElementById("email-field");
+var emailerror = document.getElementById("email-error");
+var button = document.getElementById("button");
+
+      //// for email  ////////
+function validateEmail() {
+  if(!emailfield.value.match(/^[A-Za-z\._\-0-9]+[@][A-Za-z]+[+*\.][a-z]{2,4}$/)) {
+    emailerror.innerHTML ="Valid email required";
+    emailfield.style.borderColor ="red";
+    emailfield.style.backgroundColor =" rgb(248, 200, 200)";
+    return false;
+  }
+
+  else {
+    emailerror.innerHTML ="";
+    emailfield.style.borderColor ="green";
+    emailfield.style.backgroundColor ="";
+    return true;
+  }
+}
+   
+
+//// for popup  ////////
+let popup = document.getElementById("popup");
+function openPopup(){
+  popup.classList.add("open-popup");
+}
+function closePopup(){
+  popup.classList.remove("open-popup");
+}
+
+//// for first page= to  ////////
+let page = document.getElementById("page");
+function closepage(){
+  page.classList.add("open-page");
+}
+function openpage(){
+  page.classList.remove("open-page");
+}
+
+function emailbutton() {
+  if (validateEmail() === true) {
+    closepage(), openPopup();
+  }
+}
